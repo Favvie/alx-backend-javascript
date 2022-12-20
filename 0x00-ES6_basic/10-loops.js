afd99b3e625c3bb2407ad8c00b2ab1423c5db3cb
Favvie <favourprecious3@gmail.com>
2022-12-20 02:04:33 +0100

    add readme to project directory

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+	const newarr = [];
+	for (let value of array) {
+		newarr.push(`${appendString}${value}`);
+	}
+
+	return newarr;
+}
