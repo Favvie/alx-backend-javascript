8a32a9d995d088ea08c69aa9321e41a3f49e4338
Favvie <favourprecious3@gmail.com>
2022-12-22 05:15:14 +0100

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({ status: 200, body: 'Charles' });
-    } else if (success === false) {
-      reject(Error('The fake API is not working currently'));
-    }
-  });
+	return new Promise((resolve, reject) => {
+		if (success === true) {
+			resolve({ status: 200, body: "Success" });
+		} else if (success === false) {
+			reject(Error("The fake API is not working currently"));
+		}
+	});
 }
