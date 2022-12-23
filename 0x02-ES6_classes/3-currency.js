a1d8cb9b5b74d5309db658bde540a64d5c72d86f
Favvie <favourprecious3@gmail.com>
2022-12-23 03:18:24 +0100

    [Fix] Add type checking in setter methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,34 @@
 export default class Currency {
-  constructor(code, name) {
-    this._code = code;
-    this._name = name;
-  }
+	constructor(code, name) {
+		this._code = code;
+		this._name = name;
+	}
 
-  get code() {
-    return this._code;
-  }
+	get code() {
+		return this._code;
+	}
 
-  set code(codeValue) {
-    this._code = codeValue;
-  }
+	set code(codeValue) {
+		if (typeof codeValue !== "string") {
+			throw TypeError("Code must be a string");
+		}
+		this._code = codeValue;
+	}
 
-  // name setter
+	// name setter
 
-  get name() {
-    return this._name;
-  }
+	get name() {
+		return this._name;
+	}
 
-  set name(nameValue) {
-    this._name = nameValue;
-  }
+	set name(nameValue) {
+		if (typeof nameValue !== "string") {
+			throw TypeError("Name must be a string");
+		}
+		this._name = nameValue;
+	}
 
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
+	displayFullCurrency() {
+		return `${this._name} (${this._code})`;
+	}
 }
