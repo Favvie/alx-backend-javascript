2e538ce63e8c4fc2137f0349395db84162bcb8ba
Favvie <favourprecious3@gmail.com>
2023-01-04 03:08:26 +0100

    commit file task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,10 +2,10 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw Error('Cannot process');
   }
-   const result = map.forEach((quantity, grocery) => {
+  const result = map.forEach((quantity, grocery) => {
     if (quantity === 1) {
       map.set(grocery, 100);
     }
-   });
-  return result
+  });
+  return result;
 }
