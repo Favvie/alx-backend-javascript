677c54b26e9dac0d33fcd920706da1e7dbb2b3f0
Favvie <favourprecious3@gmail.com>
2023-01-05 04:57:00 +0100

    [feat] set up html table for task 1

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -8,4 +8,27 @@ let student1: Student = {firstName: 'favour', lastName: 'Abangwu', location: 'Ab
 let student2: Student = { firstName: 'loveth', lastName: 'Chibuzor', location: 'Ekiti' }
 const studentList : Student[] = [student1, student2]
 
-const table = document.createElement('table')
+const body: HTMLBodyElement = document.querySelector("body");
+const table: HTMLTableElement = document.createElement("table");
+const thead: HTMLElement = document.createElement("thead");
+const tbody: HTMLElement = document.createElement("tbody");
+
+
+function generateRow(rowContainer: HTMLElement, data: string[]) {
+    const row: HTMLTableRowElement = document.createElement("tr");
+    rowContainer.appendChild(row);
+    for (let i = 0; i < data.length; i++) {
+        const cell: HTMLTableCellElement = document.createElement("td");
+        cell.textContent = data[i];
+        row.appendChild(cell);
+    }
+}
+
+generateRow(thead, ["First Name", "Location"]);
+for (let i = 0; i < studentList.length; i++) {
+    generateRow(tbody, [studentList[i].firstName, studentList[i].location]);
+}
+
+table.appendChild(thead);
+table.appendChild(tbody);
+body.appendChild(table);
