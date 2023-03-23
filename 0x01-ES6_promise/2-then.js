3c904645feef55bef3c056c0b37e00fd8a68c450
favvie <favourabangwu@gmail.com>
2023-03-23 14:00:41 +0100

    [fix] update finally block

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,16 @@
+/* eslint-disable arrow-body-style */
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
     .catch(() => {
-      console.log('Got a response from the API');
       return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
