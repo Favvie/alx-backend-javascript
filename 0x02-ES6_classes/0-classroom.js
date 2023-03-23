58a593d2b2331d1d9e58eb36a4675d43084b1017
favvie <favourabangwu@gmail.com>
2023-03-23 22:17:02 +0100

    [fix]  add type checking for arg: task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,15 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+
+  set maxStudentsSize(value) {
+    if (typeof value === 'number') {
+      this._maxStudentsSize = value
     }
+  }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize
+  }
 }
