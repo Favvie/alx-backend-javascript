b9873ac3f3e7d103a6f8ccb7cf446940af721886
favvie <favourabangwu@gmail.com>
2023-03-23 14:15:12 +0100

    [fix] task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,10 @@
-/* eslint-disable arrow-body-style */
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      return new Error();
-    })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => (Error()))
     .finally(() => {
       console.log('Got a response from the API');
     });
