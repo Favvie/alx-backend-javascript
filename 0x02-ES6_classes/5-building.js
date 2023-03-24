c00443bdb8d3540a3ce8dc5be1e4fad51e379b96
favvie <favourabangwu@gmail.com>
2023-03-24 12:14:27 +0100

    [feat] building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,18 @@
 export default class Building {
-    constructor(sqft) {
-        this.sqft = sqft
-    }
+  constructor(sqft) {
+    this.sqft = sqft;
 
-    set sqft(value) {
-        if (typeof value !== 'number') throw TypeError('Sqft must be a number')
-        this._sqft = value
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') throw TypeError('Sqft must be a number');
+    this._sqft = value;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
