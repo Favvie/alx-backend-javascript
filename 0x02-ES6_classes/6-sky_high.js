70eaf9f03f06e070423b79541ec6a05c48aebd0d
favvie <favourabangwu@gmail.com>
2023-03-25 04:38:56 +0100

    [feat] use the super constructor and pass a value

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,7 +1,31 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHigh extends Building {
-    constructor(sqft, floors) {
-        
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    // this.sqft = sqft;
+    this.floors = floors;
+  }
+
+  //   set sqft(value) {
+  //     if (typeof value !== 'number') throw TypeError('Sqft must be a number');
+  //     this._sqft = value;
+  //   }
+
+  //   get sqft() {
+  //     return this._sqft;
+  //   }
+
+  set floors(value) {
+    if (typeof value !== 'number') throw TypeError('Floors must be a number');
+    this._floors = value;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
