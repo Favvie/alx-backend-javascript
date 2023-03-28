c1f26730f8785d1d5be1712df284f41489556e24
favvie <favourabangwu@gmail.com>
2023-03-28 16:04:37 +0100

    [fix] typo

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8DataView = new DataView(buffer);
-  if (position >= length) throw RangeError('Postion outside range');
+  if (position >= length) throw RangeError('Position outside range');
   int8DataView.setInt8(position, value);
   return int8DataView;
 }
