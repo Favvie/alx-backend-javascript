51f3619a7f0893518f29fcb36f3acd7c50ba59da
favvie <favourabangwu@gmail.com>
2023-05-17 05:05:02 +0100

    test

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
   process.stdout.write(`Your name is: ${input}\n`);
+  process.end()
 });
 
 process.stdin.on('end', () => {
