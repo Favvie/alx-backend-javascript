e79ea8103edfb8232f929cfc46d56ee3cd98510f
favvie <favourabangwu@gmail.com>
2023-05-19 04:01:04 +0100

    test 0-c

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,18 +1,18 @@
 const assert = require('assert');
-const calculate = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul.js');
 const { it, describe } = require("mocha");
 
-describe('calculateSum', function () {
-    it('should return true of the value is rounded4 ', function () {
-        assert.equal(calculate(1, 3), 4);
-    });
-    it('should return true of the value is rounded 5', function () {
-        assert.equal(calculate(1, 3.7), 5);
-    });
-    it('should return true of the value is rounded 5', function () {
-        assert.equal(calculate(1.2, 3.7), 5);
-    });
-    it('should return true of the value is rounded 6', function () {
-        assert.equal(calculate(1.5, 3.7), 6);
-    });
+describe('calculateNumber', () => {
+  it('should return 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
