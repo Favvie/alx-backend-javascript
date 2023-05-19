efba75139789469660775433444d1a75d08fb979
favvie <favourabangwu@gmail.com>
2023-05-19 03:27:59 +0100

    add mutiple test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,16 @@ const assert = require('assert')
 const calculate = require('./0-calcul')
 
 describe('calculateSum', function () {
-    it('should return true if it equal 4', function () {
-        assert.strictEqual(calculate(1, 3), 4)
+    it('should return true of the value is rounded4 ', function () {
+        assert.equal(calculate(1, 3), 4)
+    })
+    it('should return true of the value is rounded 5', function () {
+        assert.equal(calculate(1, 3.7), 5)
+    })
+    it('should return true of the value is rounded 5', function () {
+        assert.equal(calculate(1.2, 3.7), 5)
+    })
+    it('should return true of the value is rounded 6', function () {
+        assert.equal(calculate(1.5, 3.7), 6)
     })
 })
