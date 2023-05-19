fcd23f445c7d080d59bf1487e06ee371d7dd2b01
favvie <favourabangwu@gmail.com>
2023-05-19 03:24:54 +0100

    fix 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,13 +5,4 @@ describe('calculateSum', function () {
     it('should return true if it equal 4', function () {
         assert.strictEqual(calculate(1, 3), 4)
     })
-    it('should return true if it equals 5', function () {
-        assert.strictEqual(calculate(1, 3.7), 5)
-    })
-    it('should return true if it equals 5', function () {
-        assert.strictEqual(calculate(1.2, 3.7), 5)
-    })
-    it('should return true if it equals 6', function () {
-        assert.strictEqual(calculate(1.5, 3.7), 6)
-    })
 })
