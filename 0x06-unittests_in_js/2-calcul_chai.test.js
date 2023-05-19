4c2686250cccd85d1c6e3724669a7723b6821d4b
favvie <favourabangwu@gmail.com>
2023-05-19 03:52:51 +0100

    calcul-2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,18 +1,20 @@
-const chai = require('chai')
-const expect = chai.expect
-const calculateNumber = require('./1-calcul')
+const chai = require('chai');
+const { it, describe } = require("mocha");
+const calculateNumber = require('./2-calcul_chai.js');
 
-describe('type of calculate', function () {
-    it('should calculate the arguments based on type SUM', function () {
-        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
-    })
-    it('should calculate the arguments based on type SUBTRACT', function () {
-        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4)
-    })
-    it('should calculate the arguments based on type DIVIDE', function () {
-        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
-    })
-    it('should return Error', function () {
-        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error')
-    })
-})
+const { expect } = chai;
+
+describe('calculateNumber', () => {
+  it('should return 6', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+  it('should return -4', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+  it('should return 0.2', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+  it('should return Error', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
