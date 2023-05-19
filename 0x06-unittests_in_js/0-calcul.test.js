3fbc413623a67aa9738e01fd6c58b4b30022d2dc
favvie <favourabangwu@gmail.com>
2023-05-19 03:30:49 +0100

    test 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculate = require('./0-calcul')
 
 describe('calculateSum', function () {
     it('should return true of the value is rounded4 ', function () {
-        assert.equal(calculate(1, 3), 4)
-    })
+        assert.equal(calculate(1, 3), 4);
+    });
     it('should return true of the value is rounded 5', function () {
-        assert.equal(calculate(1, 3.7), 5)
-    })
+        assert.equal(calculate(1, 3.7), 5);
+    });
     it('should return true of the value is rounded 5', function () {
-        assert.equal(calculate(1.2, 3.7), 5)
-    })
+        assert.equal(calculate(1.2, 3.7), 5);
+    });
     it('should return true of the value is rounded 6', function () {
-        assert.equal(calculate(1.5, 3.7), 6)
-    })
+        assert.equal(calculate(1.5, 3.7), 6);
+    });
 })
